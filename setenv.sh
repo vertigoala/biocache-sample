670c9fc9a7ef4d22807d594d32e45fe6909ffe50
Andre Fernandes <andre@vertigo.com.br>
2019-08-01 07:50:49 -0300

    add collectory

--- setenv.sh
+++ setenv.sh
@@ -7,3 +7,4 @@ export DOCKER_HOST=ssh://ubuntu@$SERVERNAME
 export ALA_URL=http://$SERVERNAME # traefik port
 sed "s#\$ALA_URL#$ALA_URL#g" ./config/images-config-template.properties > ./config/images-config.properties
 sed "s#\$ALA_URL#$ALA_URL#g" ./config/biocache-config-template.properties > ./config/biocache-config.properties
+sed "s#\$ALA_URL#$ALA_URL#g" ./config/ala-collectory-config-template.properties > ./config/ala-collectory-config.properties
